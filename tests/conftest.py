d077336603e46352a21f01b699481d2641ae7ad4
Daniel Kessler <dkessler@berkeley.edu>
2019-02-26 18:33:08 -0800

    use pyfakefs for validate test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,4 @@ import pytest
 @pytest.fixture(autouse=True)
 def etc(fs):
     """Use pyfakefs in every test case"""
-    fs.add_real_directory('configs', target_path='/etc/ocf')
+    fs.add_real_directory('configs', target_path='/etc/ocf', read_only=True)
