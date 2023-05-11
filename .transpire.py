44a71d9455853a3a6f0e5bb148c6b362b31d0c1d
Oliver Ni <oliver.ni@gmail.com>
2023-05-10 20:17:35 -0700

    refactor: fix pre-commit issues

--- .transpire.py
+++ .transpire.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 from transpire.types import Image
 
-name = "etc"
+name = 'etc'
 
 
 def images():
-    yield Image(name="sync", path=Path("/sync"))
+    yield Image(name='sync', path=Path('/sync'))
